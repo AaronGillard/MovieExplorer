1f7fbd1da5b5975e69dfb1e042ebb56abd6dbe6b
unknown <Aaron Gillard>
2026-01-07 05:37:41 +0000

    Added Android functionality and Theme selector

--- MovieExplorer/App.xaml.cs
+++ MovieExplorer/App.xaml.cs
@@ -1,17 +1,18 @@
-﻿namespace MovieExplorer
+﻿using MovieExplorer.Services;
+
+namespace MovieExplorer
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-
+            ThemeService.ApplySavedTheme(this);
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            //Temp disable app shell for profile picker testing
-            //return new Window(new AppShell());
+            
 
             //profile Picker verification
             return new Window(new NavigationPage(new ProfilePickerPage()));
